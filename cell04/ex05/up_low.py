951e435eee17c2e3fcf89d78fa0b527af9f2afe5
Lu Chit <168895100+lulyn33@users.noreply.github.com>
2025-06-24 18:44:56 +0700

    Create up_low.py

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,9 @@
+word = input()
+for i in word:
+    if i == i.upper():
+        i = i.lower()
+        print(i,end='')
+    else:
+        if i == i.lower():
+            i = i.upper()
+            print(i,end='')
