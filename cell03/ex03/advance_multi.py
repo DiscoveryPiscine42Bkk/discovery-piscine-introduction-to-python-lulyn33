f8f380caac11228cdfc33142dd02b2f2970e161a
Lu Chit <168895100+lulyn33@users.noreply.github.com>
2025-06-24 18:36:37 +0700

    Create advance_multi.py

---
+++ cell03/ex03/advance_multi.py
@@ -0,0 +1,12 @@
+a = 0 
+while a <= 10:
+    print(f"Table de {a} :",end=' ')
+    b = 0
+    while b <= 10:
+        rslt = a * b
+        b += 1
+        print(rslt,end=' ')
+        continue
+    a += 1
+    print('\b')     
+        
