3850ba7a0b47cdb15493d54b79552783d6c68ea4
lulyn33 <luchit888@gmail.com>
2025-06-25 01:58:57 +0700

    upload

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) <= 1:
+    print('none')
+else:
+    para = sys.argv[1]
+    if not 'z' in para:
+        print('none')
+    else:
+        for i in para:
+            if i == 'z':
+                print(i,end='')
+    
+        
