e7f375cfeca4a0aa162f37776fbe72210a71b8c6
lulyn33 <luchit888@gmail.com>
2025-06-25 01:50:23 +0700

    updated

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) != 2 :
+    print('none')
+else:
+    var = input("What was the parameter? ")
+    word = sys.argv[1]
+    if var == word:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
