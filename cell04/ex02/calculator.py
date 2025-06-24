d6cdb902c077ec5793f6abd1a549c361c44293d0
Lu Chit <168895100+lulyn33@users.noreply.github.com>
2025-06-24 18:40:04 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+first_num = int(input('Give me the first number: '))
+second_num = int(input('Give me the second number: '))
+print('Thank you!')
+
+plus = first_num + second_num 
+minus = first_num - second_num
+div = first_num // second_num
+mult = first_num * second_num
+
+print(f"{first_num} + {second_num} = {plus}")
+print(f"{first_num} - {second_num} = {minus}")
+print(f"{first_num} / {second_num} = {div}")
+print(f"{first_num} * {second_num} = {mult}")
