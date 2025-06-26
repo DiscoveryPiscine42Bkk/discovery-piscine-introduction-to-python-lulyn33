6f047a49b57b883b85d4654198416d3ef87b1552
lulyn33 <luchit888@gmail.com>
2025-06-26 15:20:54 +0700

    updated

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -1,8 +1,8 @@
 board = """\
+    .... 
     ....
-    K...
-    ..R.
-    ....\
+    .K.K
+    ..B.\
     """
     
 lst = board.split()
@@ -14,23 +14,53 @@ def checkmate(lst):
             if lst[row][col] == 'P':
                 pawn_attack(lst,row,col,size)
             elif lst[row][col] == 'R':
-                print('R')
+                rook_attack(lst,row,col,size)
             elif lst[row][col] == 'B':
-                print('B')
+                bishop_attack(lst,row,col,size)
                 
 def pawn_attack(board,row,col,size):
-    if board[row-1][col-1] == 'K':
-        # print(row,col )
-        print(size)
-        print("Success")
-    elif board[row-1][col+1] == 'K':
-        print("Success")
-    else:
-        print(size)
-        # print(board[row][col])
+    try:
+        col < size
+        if board[row-1][col-1] == 'K':
+            print("Success")
+        elif board[row-1][col+1] == 'K':
+            print("Success")
+        else:
+            print('Fail')
+            
+    except IndexError:
         print('Fail')
+#pawn attack is done        
+
+def rook_attack(board,row,col,size):
+    found = False
+    for c in range(size):
+        if board[row][c] == 'K':
+            # print(board[row][c],end=' ')
+            print('Success')
+            found = True
+            break
     
-                
+    for r in range(size):
+        if board[r][col] == 'K':
+            print('Success')           
+            found = True
+            break
+        
+    if not found:
+        print('Fail')    
+#rook attack is done
+
+def bishop_attack(board,row,col,size):
+    for r in range(size):
+        for c in range(size):
+            if board[row-r][col-c] == 'K':
+                print('Success')
+            elif c < size+1:
+                if board[row-r][col+c] == 'K':
+                    print('Success')
+            
+    
 checkmate(lst)
 
     
