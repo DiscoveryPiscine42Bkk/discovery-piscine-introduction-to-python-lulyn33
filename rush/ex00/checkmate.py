d6c784e3ca81211733c171b7a8c2a58816df3578
lulyn33 <luchit888@gmail.com>
2025-06-26 16:26:12 +0700

    updated

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -1,22 +1,26 @@
 board = """\
-    .... 
-    ....
-    .K.K
-    ..B.\
-    """
+..
+.K\
+"""
     
 lst = board.split()
 # print(lst)
 def checkmate(lst):
     size = len(lst)
     for row in range(0, len(lst)):
-        for col in range(0, len(lst)):    
-            if lst[row][col] == 'P':
-                pawn_attack(lst,row,col,size)
-            elif lst[row][col] == 'R':
-                rook_attack(lst,row,col,size)
-            elif lst[row][col] == 'B':
-                bishop_attack(lst,row,col,size)
+        for col in range(0, len(lst)):
+            if not lst[row][col] in ['P',"Q","B","R"]:
+                success = False #give one variable as success = False
+            else:       
+                if lst[row][col] == 'P':
+                    pawn_attack(lst,row,col,size)
+                elif lst[row][col] == 'R':
+                    rook_attack(lst,row,col,size)
+                elif lst[row][col] == 'B':
+                    bishop_attack(lst,row,col,size)
+                elif lst[row][col] == 'Q':
+                    queen_attack(lst,row,col,size)
+            
                 
 def pawn_attack(board,row,col,size):
     try:
@@ -36,31 +40,96 @@ def rook_attack(board,row,col,size):
     found = False
     for c in range(size):
         if board[row][c] == 'K':
-            # print(board[row][c],end=' ')
-            print('Success')
             found = True
             break
     
     for r in range(size):
-        if board[r][col] == 'K':
-            print('Success')           
+        if board[r][col] == 'K':      
             found = True
             break
         
-    if not found:
-        print('Fail')    
+    print("Success" if found else "Fail")  
 #rook attack is done
 
 def bishop_attack(board,row,col,size):
-    for r in range(size):
-        for c in range(size):
-            if board[row-r][col-c] == 'K':
-                print('Success')
-            elif c < size+1:
-                if board[row-r][col+c] == 'K':
-                    print('Success')
-            
+    found = False
     
+    r, c = row - 1, col - 1
+    while r >= 0 and c >= 0:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r -= 1
+        c -= 1
+
+    r, c = row - 1, col + 1
+    while r >= 0 and c < size:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r -= 1
+        c += 1
+
+    r, c = row + 1, col - 1
+    while r < size and c >= 0:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r += 1
+        c -= 1
+
+    r, c = row + 1, col + 1
+    while r < size and c < size:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r += 1
+        c += 1
+
+    print("Success" if found else "Fail")
+ 
+def queen_attack(board,row,col,size):
+    found = False
+    for i in range(size):
+            if board[i][col] == 'K' or board[row][i] == 'K':
+                found = True
+            else:
+                continue
+                
+    r, c = row - 1, col - 1
+    while r >= 0 and c >= 0:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r -= 1
+        c -= 1
+
+    r, c = row - 1, col + 1
+    while r >= 0 and c < size:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r -= 1
+        c += 1
+
+    r, c = row + 1, col - 1
+    while r < size and c >= 0:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r += 1
+        c -= 1
+
+    r, c = row + 1, col + 1
+    while r < size and c < size:
+        if board[r][c] == 'K':
+            found = True
+            break
+        r += 1
+        c += 1
+        
+    print("Success" if found else "Fail")
+
 checkmate(lst)
 
     
