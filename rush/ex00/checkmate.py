2598a4f88c140d4ac20233905cd9d74c5877e7dc
lulyn33 <luchit888@gmail.com>
2025-06-26 12:20:37 +0700

    pawn_only

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -1,12 +1,37 @@
 board = """\
-    R...
-    .K..
-    ..P.
+    ....
+    K...
+    ..R.
     ....\
     """
     
-print(board.split())
-
+lst = board.split()
+# print(lst)
+def checkmate(lst):
+    size = len(lst)
+    for row in range(0, len(lst)):
+        for col in range(0, len(lst)):    
+            if lst[row][col] == 'P':
+                pawn_attack(lst,row,col,size)
+            elif lst[row][col] == 'R':
+                print('R')
+            elif lst[row][col] == 'B':
+                print('B')
+                
+def pawn_attack(board,row,col,size):
+    if board[row-1][col-1] == 'K':
+        # print(row,col )
+        print(size)
+        print("Success")
+    elif board[row-1][col+1] == 'K':
+        print("Success")
+    else:
+        print(size)
+        # print(board[row][col])
+        print('Fail')
+    
+                
+checkmate(lst)
 
     
     
